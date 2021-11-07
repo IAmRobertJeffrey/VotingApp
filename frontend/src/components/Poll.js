ae13454b86da019a7cebf926cb901c4375e54c65
Rob <iamrobertjeffrey@gmail.com>
2021-11-07 18:05:20 +0000

    changed colour scheme

--- frontend/src/components/Poll.js
+++ frontend/src/components/Poll.js
@@ -1,4 +1,5 @@
 import "../styles/poll.css"
+import {BiUpvote} from 'react-icons/bi'
 
 const Poll = ({pollData}) => {
     console.log(pollData);
@@ -8,7 +9,7 @@ const Poll = ({pollData}) => {
         <div className="pollCard">
             <div className="title"><h3>{pollData.title}</h3></div>
             <div className="votes">
-            {pollData.votes.map((currentVote) => (<div key={currentVote._id} className="vote"><div className="name">{currentVote.name}</div><div className="count">{currentVote.count}</div></div>
+            {pollData.votes.map((currentVote) => (<div key={currentVote._id} className="vote"><div className="name">{currentVote.name}: <b>{currentVote.count}</b><BiUpvote className="upvote"/></div></div>
             
             
             ))}
