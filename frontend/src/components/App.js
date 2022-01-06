3df6a9fa937530246fb6e522e7c538d66067253c
Rob <iamrobertjeffrey@gmail.com>
2022-01-06 17:48:55 +0000

    removed name screen

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -6,103 +6,111 @@ import NameForm from "./NameForm";
 import AddPoll from "./AddPoll";
 import { FiPlusSquare } from "react-icons/fi";
 
-const App = () => {
-  const [data, setData] = useState();
-  const [name, setName] = useState();
-  const [nameActivated, setNameActivated] = useState();
-  const [votingOn, setVotingOn] = useState("");
-  const [addingPoll, setAddingPoll] = useState(false);
-  const [values, setValues] = useState({ val: [] });
-  const [newPollTitle, setNewPollTitle] = useState("");
+const App = () =>
+{
+	const [data, setData] = useState();
+	const [name, setName] = useState();
+	const [nameActivated, setNameActivated] = useState();
+	const [votingOn, setVotingOn] = useState("");
+	const [addingPoll, setAddingPoll] = useState(false);
+	const [values, setValues] = useState({ val: [] });
+	const [newPollTitle, setNewPollTitle] = useState("");
 
-  const fetchData = async () => {
-    const response = await fetch(
-      "https://roberts-voting-app.herokuapp.com/polls"
-    );
-    const data = await response.json();
-    setData(data);
-  };
+	const fetchData = async () =>
+	{
+		const response = await fetch(
+			"https://roberts-voting-app.herokuapp.com/polls"
+		);
+		const data = await response.json();
+		setData(data);
+	};
 
-  const sendData = async (pollObject) => {
-    const postOpt = {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(pollObject),
-    };
+	const sendData = async (pollObject) =>
+	{
+		const postOpt = {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json",
+			},
+			body: JSON.stringify(pollObject),
+		};
 
-    await fetch("https://roberts-voting-app.herokuapp.com/polls", postOpt);
+		await fetch("https://roberts-voting-app.herokuapp.com/polls", postOpt);
 
-    fetchData();
-  };
+		fetchData();
+	};
 
-  useEffect(() => {}, [votingOn]);
+	useEffect(() => { }, [votingOn]);
 
-  useEffect(() => {
-    const fetchData = async () => {
-      const response = await fetch(
-        "https://roberts-voting-app.herokuapp.com/polls"
-      );
-      const data = await response.json();
-      setData(data);
-    };
-    fetchData();
-  }, []);
+	useEffect(() =>
+	{
+		const fetchData = async () =>
+		{
+			const response = await fetch(
+				"https://roberts-voting-app.herokuapp.com/polls"
+			);
+			const data = await response.json();
+			setData(data);
+		};
+		fetchData();
+	}, []);
 
-  const handleAddPoll = () => {
-    if (addingPoll) {
-      setAddingPoll(false);
-    } else {
-      setNewPollTitle("");
-      setValues({ val: [] });
-      setAddingPoll(true);
-    }
-  };
+	const handleAddPoll = () =>
+	{
+		if (addingPoll)
+		{
+			setAddingPoll(false);
+		} else
+		{
+			setNewPollTitle("");
+			setValues({ val: [] });
+			setAddingPoll(true);
+		}
+	};
 
-  return (
-    <div className="app">
-      {nameActivated ? (
-        <header>
-          <h1 onClick={(e) => setAddingPoll(false)} className="headerTitle">
-            Public Polls
-          </h1>{" "}
-          <FiPlusSquare
-            size="50px"
-            onClick={handleAddPoll}
-            className="addPollButton pollButton"
-          />{" "}
-        </header>
-      ) : null}
+	return (
+		<div className="app">
 
-      {!nameActivated && !addingPoll ? (
-        <NameForm
-          setNameActivated={setNameActivated}
-          name={name}
-          setName={setName}
-        />
-      ) : null}
+			<header>
+				<h1 onClick={(e) => setAddingPoll(false)} className="headerTitle">
+					Public Polls
+				</h1>{" "}
+				<FiPlusSquare
+					size="50px"
+					onClick={handleAddPoll}
+					className="addPollButton pollButton"
+				/>{" "}
+			</header>
 
-      {data && nameActivated && !addingPoll ? (
-        <PollList
-          newPoll
-          setVotingOn={setVotingOn}
-          fetchData={fetchData}
-          data={data}
-        />
-      ) : null}
-      {addingPoll ? (
-        <AddPoll
-          setAddingPoll={setAddingPoll}
-          sendData={sendData}
-          newPollTitle={newPollTitle}
-          setNewPollTitle={setNewPollTitle}
-          setValues={setValues}
-          values={values}
-        />
-      ) : null}
-    </div>
-  );
+
+			{/* {!nameActivated && !addingPoll ? (
+				<NameForm
+					setNameActivated={setNameActivated}
+					name={name}
+					setName={setName}
+				/>
+			) : null} */}
+
+			{data && !addingPoll ? (
+				<PollList
+					newPoll
+					setVotingOn={setVotingOn}
+					fetchData={fetchData}
+					data={data}
+				/>
+			) : null}
+			{addingPoll ? (
+				<AddPoll
+					setAddingPoll={setAddingPoll}
+					sendData={sendData}
+					newPollTitle={newPollTitle}
+					setNewPollTitle={setNewPollTitle}
+					setValues={setValues}
+					values={values}
+				/>
+			) : null}
+		</div>
+	);
 };
 
 export default App;
